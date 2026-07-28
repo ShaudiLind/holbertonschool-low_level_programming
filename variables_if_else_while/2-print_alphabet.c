7ee49586b7ba06d6999749e5cbf9cb9156fd8076
Milenys Bonilla <14998@holbertonstudents.com>
2026-07-28 15:44:25 -0400

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - functions
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while(letter <= 'z')
+	{
+		putchar (letter);
+		letter++;
+	}
+	return (0);
+}
