4f9f9a7da74960edeed736c2ff89e5643f639612
Milenys Bonilla <14998@holbertonstudents.com>
2026-07-31 10:00:40 -0400

    print alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,23 +6,24 @@
 
 /**
  * main - Entry point
- * Description: Prints lowercase alphabet, then uppercase alphabet, followed by newline
+ * Description: Prints lowercase alphabet, then uppercase alphabet,
+ * followed by newline
  * Return: Always 0 (success)
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    /* Print lowercase letters a-z */
-    for (c = 'a'; c <= 'z'; c++)
-        putchar(c);
-    
-    /* Print uppercase letters A-Z */
-    for (c = 'A'; c <= 'Z'; c++)
-        putchar(c);
-    
-    /* Print newline */
-    putchar('\n');
+	/* Print lowercase letters a-z */
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-    return (0);
+	/* Print uppercase letters A-Z */
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	/* Print newline */
+	putchar('\n');
+
+	return (0);
 }
