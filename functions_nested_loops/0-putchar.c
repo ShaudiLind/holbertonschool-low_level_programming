5b99c3dc2cac072bcd557a420dd29de691a2a93a
Milenys Bonilla <14998@holbertonstudents.com>
2026-07-31 11:04:56 -0400

    0 putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,28 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <unistd.h>
-
-int _putchar(char c);
-
-#endif 
-/*
- * _putchar.c - Implementation of _putchar function
- */
-
-#include "main.h"
-
-/**
- * _putchar - Writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-} 
 /*
  * 0-putchar.c - Prints _putchar followed by newline
  */
