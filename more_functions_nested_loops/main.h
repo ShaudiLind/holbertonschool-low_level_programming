4da890391cbec4ed69448e4e6a94ae4498d987c4
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-03 11:06:12 -0400

    main h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+void print_most_numbers(void);
 int mul(int a, int b);
 void print_numbers(void);
 /* ... add all your other prototypes here ... */
