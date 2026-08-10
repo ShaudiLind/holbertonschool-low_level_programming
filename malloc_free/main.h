c408ca8f13d1f01389e65488675d560c665c4028
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-10 10:39:10 -0400

    main c

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -9,5 +9,6 @@ char *create_array(unsigned int size, char c);
  void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+void reset_to_98(int *n);
 #endif
 
