96efb53e947387a3182e01c5fca726b741836dd3
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-10 09:30:32 -0400

    main c cero

--- malloc_free/0-main.c
+++ malloc_free/0-main.c
@@ -1,26 +1,51 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-char *create_array(unsigned int size, char c)
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
 {
-	char *array;
 	unsigned int i;
 
-	/* Check if size is 0 */
-	if (size == 0)
-		return (NULL);
-
-	/* Allocate memory */
-	array = malloc(size * sizeof(char));
-
-	/* Check if malloc failed */
-	if (array == NULL)
-		return (NULL);
-
-	/* Initialize the array */
-	for (i = 0; i < size; i++)
-		array[i] = c;
-
-	return (array);
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *buffer;
+
+	buffer = create_array(98, 'H');
+	if  (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	simple_print_buffer(buffer, 98);
+	free(buffer);
+	return (0);
+}
