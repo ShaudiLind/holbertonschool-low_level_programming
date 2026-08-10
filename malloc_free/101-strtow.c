ce81f0e53cae900fec55197c469d111ac69f1485
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-10 10:33:32 -0400

    one houndred one

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,47 +1,73 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * argstostr - Concatenates all the arguments of the program.
- * @ac: The number of arguments.
- * @av: An array of argument strings.
- *
- * Return: Pointer to a new string, or NULL if it fails or ac is 0.
+ * word_len - Returns the length of a word.
+ * @str: String to measure.
+ * Return: Length of word.
  */
-char *argstostr(int ac, char **av)
+int word_len(char *str)
 {
-	int i, j, k, len = 0;
-	char *str;
+	int len = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+	while (str[len] && str[len] != ' ')
+		len++;
+	return (len);
+}
 
-	/* Calculate the total length needed */
-	for (i = 0; i < ac; i++)
+/**
+ * count_words - Counts number of words in string.
+ * @str: The string to analyze.
+ * Return: The number of words.
+ */
+int count_words(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i]; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-		len++; /* For the newline character */
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
 	}
+	return (count);
+}
 
-	str = malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to split.
+ * Return: Array of strings or NULL.
+ */
+char **strtow(char *str)
+{
+	char **tab;
+	int i, j, k, words;
+
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	/* Fill the new string */
-	k = 0;
-	for (i = 0; i < ac; i++)
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (words + 1));
+	if (tab == NULL)
+		return (NULL);
+	for (i = 0; i < words; i++)
 	{
-		for (j = 0; av[i][j]; j++)
+		while (*str == ' ')
+			str++;
+		k = word_len(str);
+		tab[i] = malloc(sizeof(char) * (k + 1));
+		if (tab[i] == NULL)
 		{
-			str[k] = av[i][j];
-			k++;
+			for (j = 0; j < i; j++)
+				free(tab[j]);
+			free(tab);
+			return (NULL);
 		}
-		str[k] = '\n';
-		k++;
+		for (j = 0; j < k; j++)
+			tab[i][j] = *str++;
+		tab[i][j] = '\0';
 	}
-	str[k] = '\0';
-
-	return (str);
+	tab[i] = NULL;
+	return (tab);
 }
 
