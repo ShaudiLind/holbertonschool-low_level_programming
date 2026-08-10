be4a3acc8991507e2b74a883b60405fd9d8f8c62
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-10 10:49:10 -0400

    main h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,6 +8,9 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
+void reset_to_98(int *n);
+void puts2(char *str);
 /* Add other function prototypes here as you complete more tasks */
 
 #endif
