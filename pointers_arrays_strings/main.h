2cfdb61d3655c0ca545352e18c8ef257fcdfed97
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-17 10:11:15 -0400

    main h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,8 +15,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _atoi(char *s);.
-/* Add other function prototypes here as you complete more tasks */
-
 #endif
-
