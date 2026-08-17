bd7de4c82e69ee440fcfa9873e349bf723ce9e1a
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-17 10:51:27 -0400

    atoi houndred

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -22,7 +22,6 @@ int _atoi(char *s)
 		len++;
 
 	while (i < len && f== 0)
-
 	{
 		if (s[i] == '-')
 			++d;
@@ -34,7 +33,7 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i = 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
@@ -45,5 +44,4 @@ int _atoi(char *s)
 		return (0);
 
 	return (n);
-
 }
