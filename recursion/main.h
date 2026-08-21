23e3d70a5334f36113edede3a2992a1238ea7b93
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-21 17:53:32 -0400

    main

---
+++ recursion/main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function to print a single character */
+int _putchar(char c);
+
+/* Task 0 prototype */
+void _puts_recursion(char *s);
+
+/* Task 1 prototype */
+void _print_rev_recursion(char *s);
+
+/* Task 2 prototype*/
+int _strlen_recursion(char *s);
+
+/*Task 3 prototype*/
+int factorial(int n);
+
+/* Task 4 prototype*/
+int _pow_recursion(int x, int y);
+
+/* Task 5 prototype*/
+int _sqrt_recursion(int n);
+
+/* Task 6 prototype */
+int is_prime_number(int n);
+
+
+/* Add prototypes for your future tasks here, for example: */
+/* void _print_rev_recursion(char *s); */
+/* int _strlen_recursion(char *s); */
+
+#endif /* MAIN_H */
+
