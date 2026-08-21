524b4f0a9c2a08637938677bf72dd71dd6b5e141
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-21 14:23:20 -0400

    function pointers

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+
+/* prototype for _putchar */
+int _putchar(char c);
+
+/* prototype for task 0 */
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
+
