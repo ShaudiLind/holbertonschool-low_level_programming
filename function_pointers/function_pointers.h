c59bf800d3adcd0294b34af9707f91a7c627da0d
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-21 14:25:09 -0400

    functions pointers

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -8,6 +8,6 @@ int _putchar(char c);
 
 /* prototype for task 0 */
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* FUNCTION_POINTERS_H */
 
