c0777da682b9d3c2715753b4d377d36843a6fa78
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-21 16:02:26 -0400

    strchr two

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,19 +12,16 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-
 	/* Iterate through the string until the null terminator is reached */
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		/* If the current character matches c, return a pointer to it */
 		if (s[i] == c)
 			return (s + i);
-
 		/* If we reached the null terminator and didn't find c, stop */
 		if (s[i] == '\0')
 			break;
 	}
-
 	return (NULL);
 }
 
