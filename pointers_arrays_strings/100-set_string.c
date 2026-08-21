1798cf04fcf0daee35a8bd95f5922f389ba3048a
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-21 16:47:10 -0400

    set string houndred

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to the pointer we want to modify
+ * @to: pointer to the string we want to point to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
