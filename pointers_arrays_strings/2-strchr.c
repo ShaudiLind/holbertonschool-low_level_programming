ca5150aa8c24658faa88d535cbdaaef9839c745c
Milenys Bonilla <14998@holbertonstudents.com>
2026-08-21 15:59:40 -0400

    strchr two

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,30 @@
 #include "main.h"
+#include <stddef.h> /* Included for NULL */
 
 /**
  * _strchr - locates a character in a string
- * @s: string to search
- * @c: character to find
+ * @s: the string to search
+ * @c: the character to find
  *
- * Return: pointer to the first occurrence of c, or NULL
+ * Return: a pointer to the first occurrence of the character c in the string s,
+ *         or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int i;
+
+	/* Iterate through the string until the null terminator is reached */
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		/* If the current character matches c, return a pointer to it */
+		if (s[i] == c)
+			return (s + i);
+
+		/* If we reached the null terminator and didn't find c, stop */
+		if (s[i] == '\0')
+			break;
 	}
-	if (c == '\0')
-		return (s);
+
 	return (NULL);
 }
 
