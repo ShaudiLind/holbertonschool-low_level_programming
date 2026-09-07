69da635c7b20154e7defc480e4d53fc69189237f
Milenys Bonilla <14998@holbertonstudents.com>
2026-09-07 19:20:19 -0400

    variadic function

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Prototype for the mandatory _putchar function */
+int _putchar(char c);
+
+/* Prototype for Task 0 */
+int sum_them_all(const unsigned int n, ...);
+
+/* 
+ * You will add the prototypes for Tasks 1, 2, and 3 here 
+ * as you progress through the project.
+ */
+
+#endif /* VARIADIC_FUNCTIONS_H */
+
