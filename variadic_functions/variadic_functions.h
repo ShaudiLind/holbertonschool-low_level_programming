4ecc0a159626544d53f64133ebf0b16338954b04
Milenys Bonilla <14998@holbertonstudents.com>
2026-09-07 19:31:26 -0400

    variadic functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,6 +9,12 @@ int _putchar(char c);
 /* Prototype for Task 0 */
 int sum_them_all(const unsigned int n, ...);
 
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
 /* 
  * You will add the prototypes for Tasks 1, 2, and 3 here 
  * as you progress through the project.
